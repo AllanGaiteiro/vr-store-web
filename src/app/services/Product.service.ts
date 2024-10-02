a7b5406e27ec147e42e9043966114f12d8521b8a
allangaiteiro <allansgaiteiro@gmail.com>
2024-10-02 08:47:42 -0300

    feat: create build Params by getProducst

--- src/app/services/Product.service.ts
+++ src/app/services/Product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable, of, Subject } from 'rxjs';
 import { Product } from '../models/Product';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ProductFilter } from '../models/ProductFilter';
 
 @Injectable({
@@ -15,7 +15,32 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getProducts(filter?: ProductFilter): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.apiUrl}/products`);
+    // Constrói a URL com parâmetros de consulta se o filtro for fornecido
+    const params = filter ? { params: this.buildParams(filter) } : {};
+    return this.http.get<Product[]>(`${this.apiUrl}/products`, params);
+  }
+
+  private buildParams(filter: ProductFilter): HttpParams {
+    let params = new HttpParams();
+
+    if (filter.id) {
+      params = params.set('productId', filter.id.toString());
+    }
+
+    if (filter.description) {
+      console.log(filter.description)
+      params =  params.set('description', filter.description.toString());
+    }
+
+    if (filter.cost) {
+      params =  params.set('minCost', filter.cost);
+    }
+    /*
+    if (filter.maxCost) {
+      params.maxCost = filter.maxCost; // Usando o custo máximo do filtro
+    }
+      */
+    return params;
   }
 
   getProduct(id: number): Observable<Product> {
@@ -38,15 +63,16 @@ export class ProductService {
   }
 
   deleteProduct(productId: number): Promise<void> {
-    return firstValueFrom(this.http.delete<void>(`${this.apiUrl}/products/${productId}`))
-      .then(() => {
-        // Notifica que os produtos mudaram após a exclusão
-        this.fetchProducts(); // Chame um método para reobter os produtos
-      });
+    return firstValueFrom(
+      this.http.delete<void>(`${this.apiUrl}/products/${productId}`)
+    ).then(() => {
+      // Notifica que os produtos mudaram após a exclusão
+      this.fetchProducts(); // Chame um método para reobter os produtos
+    });
   }
 
   private fetchProducts(): void {
-    this.getProducts().subscribe(products => {
+    this.getProducts().subscribe((products) => {
       this.productsSubject.next(products); // Emite a lista atualizada
     });
   }
