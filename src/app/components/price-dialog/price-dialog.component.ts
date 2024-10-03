47bba46e1af7fae9b78b2c0b4b9206b6fab09bcd
allangaiteiro <allansgaiteiro@gmail.com>
2024-10-03 00:24:22 -0300

    feat: create component  price dialog

---
+++ src/app/components/price-dialog/price-dialog.component.ts
@@ -0,0 +1,84 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { ToastService } from '../../services/toast.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { StoreService } from '../../services/store.service';
+import { PriceService } from '../../services/price.service';
+import { Price } from '../../models/Price';
+import { Store } from '../../models/Store';
+import { PriceFormComponent } from '../forms/price-form/price-form.component';
+import { PriceFormService } from '../../services/price-form.service';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-price-dialog',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    PriceFormComponent,
+],
+  templateUrl: './price-dialog.component.html',
+  styleUrls: ['./price-dialog.component.scss'],
+  providers: [ToastService, PriceService, StoreService],
+})
+export class PriceDialogComponent implements OnInit {
+  priceForm!: FormGroup;
+  stores?: Store[];
+  isEditMode: boolean = false;
+
+  constructor(
+    private dialogRef: MatDialogRef<PriceDialogComponent>,
+    private toastService: ToastService,
+    private priceService: PriceService,
+    private storeService: StoreService,
+    private priceFormService: PriceFormService,
+    @Inject(MAT_DIALOG_DATA) public data: Price | null
+  ) {
+    this.priceForm = this.priceFormService.getFormGroup();
+  }
+
+  ngOnInit(): void {
+
+    this.storeService.getStores().subscribe((res) => {
+      this.stores = res;
+    });
+
+    if (this.data?.id) {
+      this.isEditMode = true;
+      this.priceFormService.updateFormValues({
+        store: this.data.store,
+        priceValue: this.data.priceValue,
+      });
+    }
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    if (this.priceForm.valid) {
+      if (this.isEditMode) {
+        const updatedPrice = { ...this.data, ...this.priceForm.value };
+        this.priceService.updatePrice(updatedPrice).then((res) => {
+          this.dialogRef.close(res);
+        });
+      } else {
+        this.priceService.createPrice(this.priceForm.value).then((res) => {
+          this.dialogRef.close(res);
+        });
+      }
+    } else {
+      this.toastService.show(
+        'Um ou mais campos obrigatórios não foram preenchidos corretamente.'
+      );
+    }
+  }
+}
