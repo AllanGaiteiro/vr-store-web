6c3efeb69e35f6f21945c60530a75c6da96141ae
allangaiteiro <allansgaiteiro@gmail.com>
2024-10-04 01:27:02 -0300

    feat: create component error

---
+++ src/app/components/error-list/error-list.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-list',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div *ngIf="hasError" class="error-message">
+      Ocorreu um erro ao carregar os produtos. Tente novamente.
+    </div>
+  `,
+  styleUrl: './error-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ErrorListComponent {
+  @Input() hasError?:boolean;
+}
---
+++ src/app/components/not-found/not-found.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div *ngIf="notFound" class="no-products-message">
+      <p>Nenhum produto encontrado.</p>
+    </div>
+  `,
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {
+  @Input() notFound?: boolean;
+}
