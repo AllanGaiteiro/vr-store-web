7b08e920b91a456369b59d1c2ff5c60d14291847
allangaiteiro <allansgaiteiro@gmail.com>
2024-10-04 13:05:29 -0300

    feat: update products before delete

--- src/app/components/buttons/delete-button/delete-product-button.component.ts
+++ src/app/components/buttons/delete-button/delete-product-button.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProductService } from '../../../services/product.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-product-button',
@@ -22,13 +23,14 @@ export class DeleteProductButtonComponent {
   @Output() deleted = new EventEmitter<void>();
   faTrash = faTrash;
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   deleteProduct(productId: number): void {
     if (confirm('Tem certeza que deseja deletar este produto?')) {
       this.productService.deleteProduct(productId).then(() => {
         console.log('produto deletado com sucesso');
         this.deleted.emit();
+        this.router.navigate(['/produto']);
       });
     }
   }
